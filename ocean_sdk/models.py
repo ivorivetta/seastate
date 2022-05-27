703cf83d1e50dbe09d687b0898c650282a39582c
Ivo <ivorivetta@users.noreply.github.com>
2022-05-27 20:42:26 +0200

    added logging and comments

--- ocean_sdk/models.py
+++ ocean_sdk/models.py
@@ -2,6 +2,13 @@ from typing import List, Dict
 
 class Result:
     def __init__(self, status_code: int, message: str = '', data: List[Dict] = None):
+        """Facade for requests.Response 
+
+        Args:
+            status_code (int): HTTP Status
+            message (str, optional): Defaults to ''.
+            data (List[Dict], optional): Defaults to None.
+        """
         self.status_code = int(status_code)
         self.message = str(message)
         self.data = data if data else []
--- ocean_sdk/rest_adapter.py
+++ ocean_sdk/rest_adapter.py
@@ -1,14 +1,23 @@
+import logging
 import requests
 import requests.packages
 from typing import List, Dict
-
-from ocean_sdk.exceptions import OceanSDKException
-from ocean_sdk.models import Result
-
 from json import JSONDecodeError
+from exceptions import OceanSDKException
+from models import Result
 
 class RestAdapter:
-    def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True):
+    def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True, logger: logging.Logger = None):
+        """Constructor for RestAdapter
+
+        Args:
+            hostname (str): Meant for NOAA and Weather.gov APIs
+            api_key (str, optional): todo. Defaults to ''.
+            ver (str, optional): Defaults to 'v1'.
+            ssl_verify (bool, optional): Defaults to True.
+            logger (logging.Logger, optional): Pass explictly else will be creted with __name__.
+        """
+        self._logger = logger or logging.getLogger(__name__)
         self.url = "https://{}/{}/".format(hostname, ver)
         self._api_key = api_key
         self._ssl_verify = ssl_verify
@@ -18,7 +27,11 @@ class RestAdapter:
     def _do(self, http_method, endpoint:str, ep_params: Dict = None, data: Dict = None ) -> Result:
         full_url = self.url + endpoint
         headers = {'x-api-key': self._api_key}
+        log_line_pre = f"method={http_method}, url={full_url}, params={ep_params}"
+        log_line_post = ', '.join((log_line_pre, "success={}, status_code={}, message={}"))
+        # Log HTTP params and try HTTP request
         try:
+            self._logger.debug(msg=log_line_pre)
             response = requests.request(
                 method=http_method,
                 url=full_url,
@@ -27,14 +40,35 @@ class RestAdapter:
                 params=ep_params,
                 json=data)
         except requests.exceptions.requests.RequestException as e:
-            raise OceanSDKException("NOAA Request Failed") from e
+            self._logger.error(msg=str(e))
+            raise OceanSDKException("Request Failed") from e
+        # Parse JSON ouput to Python object or return failed Result on exception
         try:
             data_out = response.json()
         except (ValueError, JSONDecodeError) as e:
-            raise OceanSDKException()
-        if 299 >= response.status_code >= 200:
+            self._logger.error(msg=log_line_post.format(False, None, e))
+            raise OceanSDKException("Bad JSON in Response") from e
+        is_success = 299 >= response.status_code >= 200
+        log_line = log_line_post.format(is_success, response.status_code, response.reason)
+        # if status code in 200-299 range, return Result, else raise exception
+        if is_success:
+            self._logger.debug(msg=log_line)
             return Result(response.status_code, message=response.reason, data=data_out)
+        self._logger.error(msg=log_line)
         raise OceanSDKException(f"{response.status_code}: {response.reason}")
-            
+
+
     def get(self, endpoint: str, ep_params: Dict = None) -> Result:
+        """GET method for RestAdapter
+
+        Args:
+            endpoint (str): target endpoint
+            ep_params (Dict, optional): key:value API parameters. Defaults to None.
+
+        Returns:
+            Result: Simplified Response object with:
+                status_code
+                message
+                data
+        """
         return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
